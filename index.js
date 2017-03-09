df28872fd5ff68138950e452ab19315c2319e39a
Vishnu T Suresh <vishnutsuresh@live.com>
2017-03-10 03:28:40 +0530

    settings outside

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const spawn = require('child_process').spawn;
 const open = require('open');
 const moment = require('moment');
+const jsonfile = require('jsonfile');
 const _ = require('lodash');
 
 let _time=_.memoize(function(hour,minute){
@@ -10,14 +11,18 @@ function time(hour,minute){
     return _time(hour,minute,moment().dayOfYear(),moment().year());
 };
 
+let settings=jsonfile.readFileSync("settings.json");
+let from=settings.from;
+let to=settings.to;
+
 let popup_showed=false;
 let app_start_time=moment();
 
 function tick() {
     const now_time=moment();
     if(now_time.diff(app_start_time,"minutes")>=1){
-        const shutdown_time=time(23,0);
-        const wakeup_time=time(6,0);
+        const shutdown_time=time(from.hour,from.minute);
+        const wakeup_time=time(to.hour,to.minute);
         const popup_time=shutdown_time.clone().subtract(15,"minutes");
 
         if(now_time.isBetween(popup_time,shutdown_time)){
